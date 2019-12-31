3a8e0b19b1e392a025ee1903c0c2566a8b1a9eca
susanto-tm <susanto.tm@gmail.com>
2019-12-31 14:11:29 +0700

    Finished solver

--- Sudoku_Solver/sudoku.py
+++ Sudoku_Solver/sudoku.py
@@ -1,20 +1,68 @@
 import numpy as np
 
-board = [[3, 0, 1],
-        [0, 0, 2],
-        [1, 2, 0]]
+board = [[7, 8, 0, 4, 0, 0, 1, 2, 0],
+         [6, 0, 0, 0, 7, 5, 0, 0, 9],
+         [0, 0, 0, 6, 0, 1, 0, 7, 8],
+         [0, 0, 7, 0, 4, 0, 2, 6, 0],
+         [0, 0, 1, 0, 5, 0, 9, 3, 0],
+         [9, 0, 4, 0, 6, 0, 0, 0, 5],
+         [0, 7, 0, 3, 0, 0, 0, 1, 2],
+         [1, 2, 0, 0, 0, 7, 4, 0, 0],
+         [0, 4, 9, 2, 0, 6, 0, 0, 7]]
+
+
+def solve(bo):
+    find = find_empty(bo)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1, 10):
+        if valid(bo, i, (row, col)):
+            bo[row][col] = i
+
+            if solve(bo):
+                return True
+
+            bo[row][col] = 0
+
+    return False
+
+
+def valid(bo, num, pos):
+    # check rows
+    for i in range(len(bo[0])):
+        if bo[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    # check column
+    for i in range(len(bo)):
+        if bo[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    # check 3x3
+    box_x = pos[1] // 3  # (3, 3)  -- 1, 1 -- 3, 6 ---j = 3,
+    box_y = pos[0] // 3
+
+    for i in range(box_y*3, box_y*3 + 3):
+        for j in range(box_x*3, box_x*3 + 3):
+            if bo[i][j] == num and (i, j) != pos:
+                return False
+
+    return True
 
 
 def print_board(bo):
     for i in range(len(bo)):
-        if i % 1 == 0 and i != 0:
-            print("-------------")
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - -")
 
         for j in range(len(bo[0])):
-            if j % 1 == 0 and j != 0:
+            if j % 3 == 0 and j != 0:
                 print(" | ", end="")
 
-            if j == 2:
+            if j == 8:
                 print(bo[i][j])
             else:
                 print(str(bo[i][j]) + " ", end="")
@@ -28,3 +76,8 @@ def find_empty(bo):
 
     return None
 
+
+print_board(board)
+solve(board)
+print("\n---------------------------------------\n")
+print_board(board)
